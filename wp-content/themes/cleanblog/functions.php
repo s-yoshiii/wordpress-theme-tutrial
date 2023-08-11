54a33cc548e8b69c9574acd24fd4ef3e2d0f0dde
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-08-12 00:08:18 +0900

    メニューを作成しよう

--- wp-content/themes/cleanblog/functions.php
+++ wp-content/themes/cleanblog/functions.php
@@ -1,14 +1,20 @@
-<?php 
-add_action('init', function() {
-    add_theme_support('post-thumbnails');
+<?php
+add_action('init', function () {
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  // メニューをサポート
+  register_nav_menus([
+    'global_nave' => 'グローバルナビゲーション'
+  ]);
 });
 /* アイキャッチ画像がなければ、標準画像を取得する */
-function get_eycatch_with_default () {
-    if (has_post_thumbnail()) :
-        $id = get_post_thumbnail_id();
-        $img = wp_get_attachment_image_src($id, 'large');
-      else:
-        $img = array(get_template_directory_uri() . '/img/post-bg.jpg');
-      endif;
-      return $img;
+function get_eycatch_with_default()
+{
+  if (has_post_thumbnail()) :
+    $id = get_post_thumbnail_id();
+    $img = wp_get_attachment_image_src($id, 'large');
+  else :
+    $img = array(get_template_directory_uri() . '/img/post-bg.jpg');
+  endif;
+  return $img;
 }
