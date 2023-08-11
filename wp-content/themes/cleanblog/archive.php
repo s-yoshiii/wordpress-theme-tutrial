366004c69e1cca52347e5ea6b9f4f923dd3680f9
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-08-11 18:47:06 +0900

    初期ファイルコミット

---
+++ wp-content/themes/cleanblog/archive.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<?php get_header(); ?>
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="post.html">Sample Post</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <?php if(is_category()): ?>
+            <h1>Category</h1>
+            <?php elseif(is_author()): ?>
+              <h1>Auther</h1>
+            <?php elseif(is_date()): ?>
+              <h1>Date</h1>
+            <?php else: ?>
+              <h1>Tag</h1>
+            <?php endif; ?>
+            <span class="subheading"><?php wp_title(''); ?></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <?php if (have_posts()) : ?>
+          <?php while (have_posts()) : the_post(); ?>
+
+            <div class="post-preview">
+              <a href="<?php the_permalink(); ?>">
+                <h2 class="post-title">
+                  <?php the_title(); ?>
+                </h2>
+                <h3 class="post-subtitle">
+                  <?php the_excerpt(); ?>
+                </h3>
+              </a>
+              <p class="post-meta">Posted by
+                <?php the_author(); ?>
+                <?php the_time(get_option('date_format')); ?>
+              </p>
+            </div>
+            <hr>
+          <?php endwhile; ?>
+          <!-- Pager -->
+          <!-- Pager -->
+          <div class="clearfix">
+            <?php 
+              $prev_link = get_previous_posts_link('&larr; 新しい記事へ');
+              if($prev_link) {
+                $prev_link = str_replace('<a','<a class="btn btn-primary float-left"',$prev_link);
+                echo $prev_link;
+              }
+            ?>
+            <?php 
+              $next_link = get_next_posts_link('古い記事へ &rarr;');
+              if($next_link) {
+                $next_link = str_replace('<a','<a class="btn btn-primary float-right"',$next_link);
+                echo $next_link;
+              }
+            ?>
+          </div>
+        <?php else : ?>
+          <p>記事が見つかりませんでした。</p>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+
+ <?php get_template_part('./includes/footer') ?>
+<?php get_footer(); ?>
+</body>
+
+</html>
---
+++ wp-content/themes/cleanblog/single.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <?php get_header(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+  <?php get_template_part('./includes/header'); ?>
+  <?php 
+  $num = 1;
+  echo $num;
+  ?>
+  <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <!-- Page Header -->
+      <?php
+      $img = get_eycatch_with_default();
+      ?>
+      <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <div class="post-heading">
+                <h1>
+                  <?php the_title(); ?>
+                </h1>
+                <span class="meta">Posted by
+                  <a href="#">Start Bootstrap</a>
+                  <?php the_date() ?></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <!-- Post Content -->
+      <article>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <?php the_post_thumbnail(array(32, 32), array('alt' => 'アイキャッチ画像')); ?>
+              <?php the_content() ?>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <hr>
+    <?php endwhile; ?>
+  <?php endif; ?>
+  <?php get_template_part('./includes/footer') ?>
+  <?php get_footer(); ?>
+
+</body>
+
+</html>
