f712a1e347b0142eefc5b8e26ecbe2d70f5cc2bc
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-08-12 23:29:30 +0900

    ブログサイトを仕上げよう

--- wp-content/themes/cleanblog/index.php
+++ wp-content/themes/cleanblog/index.php
@@ -6,33 +6,7 @@
 </head>
 
 <body>
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.html">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="post.html">Sample Post</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+<?php get_template_part('./includes/header'); ?>
 
   <!-- Page Header -->
   <header class="masthead" style="background-image: url('img/home-bg.jpg')">
@@ -41,8 +15,8 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>Clean Blog <?php echo date('n'); ?></h1>
-            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            <h1><?php bloginfo('name'); ?></h1>
+            <span class="subheading"><?php bloginfo('description'); ?></span>
           </div>
         </div>
       </div>
