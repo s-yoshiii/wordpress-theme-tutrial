f0e75dc0f35d24432a9dfa3ec5a91c475bdf04f6
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-08-17 00:00:56 +0900

    カスタム投稿タイプを登録しよう

--- wp-content/themes/underscores/functions.php
+++ wp-content/themes/underscores/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * underscores functions and definitions
  *
@@ -7,10 +8,21 @@
  * @package underscores
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
+
+add_action('init', function () {
+	register_post_type('item', [
+		'label' => '商品',
+		'public' => true,
+		'menu_position' => 10,
+		'supports' =>  ['title', 'thumbnail', 'editor'],
+		'menu_icon' => 'dashicons-store'
+	]);
+});
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -19,17 +31,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function underscores_setup() {
+function underscores_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on underscores, use a find and replace
 		* to change 'underscores' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'underscores', get_template_directory() . '/languages' );
+	load_theme_textdomain('underscores', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +50,19 @@ function underscores_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'underscores' ),
+			'menu-1' => esc_html__('Primary', 'underscores'),
 		)
 	);
 
@@ -83,7 +96,7 @@ function underscores_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +113,7 @@ function underscores_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'underscores_setup' );
+add_action('after_setup_theme', 'underscores_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +122,24 @@ add_action( 'after_setup_theme', 'underscores_setup' );
  *
  * @global int $content_width
  */
-function underscores_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'underscores_content_width', 640 );
+function underscores_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('underscores_content_width', 640);
 }
-add_action( 'after_setup_theme', 'underscores_content_width', 0 );
+add_action('after_setup_theme', 'underscores_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function underscores_widgets_init() {
+function underscores_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'underscores' ),
+			'name'          => esc_html__('Sidebar', 'underscores'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'underscores' ),
+			'description'   => esc_html__('Add widgets here.', 'underscores'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +147,23 @@ function underscores_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'underscores_widgets_init' );
+add_action('widgets_init', 'underscores_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function underscores_scripts() {
-	wp_enqueue_style( 'underscores-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'underscores-style', 'rtl', 'replace' );
+function underscores_scripts()
+{
+	wp_enqueue_style('underscores-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('underscores-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'underscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('underscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'underscores_scripts' );
+add_action('wp_enqueue_scripts', 'underscores_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +188,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
